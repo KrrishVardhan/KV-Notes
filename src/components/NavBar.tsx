a99bdb3dbaf77e09e4023e01ba42d2edb7b7a2e3
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2025-12-22 20:29:29 +0530

    adding sidebar for updates

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/ui/8bit/badge"
 
 function NavBar() {
     return (
-        <div className="flex retro justify-between border-b-3 border-black bg-gray-100 px-10 py-5">
+        <div className="flex retro justify-between border-b border-dashed border-gray-300  bg-gray-100 px-10 py-5">
             <h1 className="text-2xl">KV-Notes</h1>
             <Badge className="hidden md:block">B-Section Only</Badge>
         </div>
